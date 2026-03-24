37d85d0c3710e9ca6aba7ec96908e3cbf0c97343
Jack <253621235+Captoftheblackpearl@users.noreply.github.com>
2026-03-24 14:18:09 -0700

    Update next.config.js for export output and images

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    output: 'export',         
+    images: { unoptimized: true },
     env: {
         stackbitPreview: process.env.STACKBIT_PREVIEW
     },
     trailingSlash: true,
     reactStrictMode: true,
+    // allowedDevOrigins is for local dev, we can keep it
     allowedDevOrigins: [
         '192.168.1.84'
     ]
